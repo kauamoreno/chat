fda1d900a0d76025808103a6425e7609ca5550ce
Aluno <kaua.moreno2005@gmail.com>
2023-04-24 10:16:48 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//Importar o módulo express
+const express = require('express');
+const app = express();
+
+//Importar o módulo HTTP e atribuindo a constante do express e criando um servidor
+const http = require('http').createServer(app);
+
+//Importar o socket.io e colocando o http como parâmetro
+const io = require('socket.io')(http);
+
+app.use('/static', express.static('public'));
+
+//Rota para página inicial
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+//Evento para o usuário conectar ao servidor
+io.on('connection', (socket) => {
+    console.log("Um novo Usuário foi conectado")
+    //Evento para quando o usuario enviar uma mensagem via socket.io
+    socket.on('chat message', (dados) => io.emit('chat message', dados));
+
+    //Evento para quando o usuário se desconectar
+    socket.on('disconnect', () => console.log("Um Usuário foi desconectado"));
+})
+
+//Iniciar o servidor
+const port = 8080;
+http.listen(port, () => {
+    console.log(`Servidor iniciado: http://localhost:${port}`);
+})
---
+++ public/js/script.js
@@ -0,0 +1,52 @@
+//Cria uma instância do Socket.IO
+const socket = io()
+
+//Seleciona os input
+const nomeInput = document.getElementById('nome');
+const mensagemInput = document.getElementById('mensagem');
+const mensagens = document.getElementById('mensagens');
+
+//Adiciona um evento para o envio do formulário
+document.querySelector('form').addEventListener('submit', evento => {
+    const nome = nomeInput.value;
+    const mensagem = mensagemInput.value;
+
+    evento.preventDefault(); //Previne o envio padrão do formulário para não atualizar a página
+
+    //Método JAVASCRIPT que verifica os valores válido (Não está em branco os campos)
+    //EMIT envia um evento chamado "chat message" com um objeto contendo os valores
+    //TRIM() é um método que remove os espaços em branco do inicio ao fim de uma string
+    nome.trim() && mensagem.trim() && socket.emit('chat message', { nome, mensagem });
+
+    //Limpa o input da mensagem e desabilita o input do nome do usuário após a primeira mensagem
+    mensagemInput.value = '';
+    nomeInput.disabled = true;
+
+    // Conectando com o banco de dados e usando o metodo POST
+    fetch("http://localhost:3000/mensagens", {
+        method: "POST",
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            mensagem: mensagem
+        })
+    }).then(response => response.json())
+});
+
+//Adiciona um evento de mensagem recebido para o servidor
+socket.on('chat message', dados => {
+    //Cria um elemento de lista para exibir a mensagem
+    const lista = document.createElement('li');
+    //Atribuir uma ID de acordo com o seu nome de usuário
+    if (dados.nome === nomeInput.value) {
+        lista.setAttribute('id', 'usuario')
+    } else {
+        lista.setAttribute('id', 'outro')
+    }
+    //Define o texto da mensagem
+    lista.textContent = `${dados.nome}: ${dados.mensagem}`;
+    //Adiciona o elemento de mensagens
+    mensagens.appendChild(lista);
+})
