948d6547edaff5b81427b851ea56f7367c6353f2
Aluno <kaua.moreno2005@gmail.com>
2023-04-29 15:52:04 -0300

    utilizando o socket.io

--- public/js/script.js
+++ public/js/script.js
@@ -2,13 +2,13 @@
 const socket = io()
 
 //Seleciona os input
-const nomeInput = document.getElementById('nome');
-const mensagemInput = document.getElementById('mensagem');
+//const nomeInput = document.getElementById('nome');
+const mensagemInput = document.getElementById('mensagemInput');
 const mensagens = document.getElementById('mensagens');
 
 //Adiciona um evento para o envio do formulário
-document.querySelector('form').addEventListener('submit', evento => {
-    const nome = nomeInput.value;
+document.querySelector('#enviar').addEventListener('click', evento => {
+    //const nome = nomeInput.value;
     const mensagem = mensagemInput.value;
 
     evento.preventDefault(); //Previne o envio padrão do formulário para não atualizar a página
@@ -16,11 +16,11 @@ document.querySelector('form').addEventListener('submit', evento => {
     //Método JAVASCRIPT que verifica os valores válido (Não está em branco os campos)
     //EMIT envia um evento chamado "chat message" com um objeto contendo os valores
     //TRIM() é um método que remove os espaços em branco do inicio ao fim de uma string
-    nome.trim() && mensagem.trim() && socket.emit('chat message', { nome, mensagem });
+    mensagem.trim() && socket.emit('chat message', { nome, mensagem });
 
     //Limpa o input da mensagem e desabilita o input do nome do usuário após a primeira mensagem
     mensagemInput.value = '';
-    nomeInput.disabled = true;
+    // nomeInput.disabled = true;
 });
 
 //Adiciona um evento de mensagem recebido para o servidor
@@ -28,13 +28,18 @@ socket.on('chat message', dados => {
     //Cria um elemento de lista para exibir a mensagem
     const lista = document.createElement('li');
     //Atribuir uma ID de acordo com o seu nome de usuário
-    if (dados.nome === nomeInput.value) {
-        lista.setAttribute('id', 'usuario')
-    } else {
-        lista.setAttribute('id', 'outro')
-    }
+    
+    
+    
+    // if (dados.nome === nomeInput.value) {
+    //     lista.setAttribute('id', 'usuario')
+    // } else {
+    //     lista.setAttribute('id', 'outro')
+    // }
+
+
     //Define o texto da mensagem
-    lista.textContent = `${dados.nome}: ${dados.mensagem}`;
+    lista.textContent = `${dados.mensagem}`;
     //Adiciona o elemento de mensagens
     mensagens.appendChild(lista);
 })
